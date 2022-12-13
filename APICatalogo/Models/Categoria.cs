b1095254fd1942c45c551b9a9e8412b035ac1132
Gustavo Figueiroa <gustavofsoledade@hotmail.com>
2022-12-13 15:42:35 -0300

    Definindo relacionamento entre as entidades

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,7 +1,17 @@
-﻿namespace APICatalogo.Models;
+﻿using System.Collections.ObjectModel;
+
+namespace APICatalogo.Models;
 public class Categoria
 {
     public int CategoriaId { get; set; }
     public string? Nome { get; set; }
-    public string? ImagemURL { get; set; }
+    public string? ImagemUrl { get; set; }
+
+    public ICollection<Produto>? Produtos { get; set; }
+
+    //Boa pratica iniciar a coleção assim que definirmos uma coleção na classe.
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
 }
--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -5,8 +5,10 @@ public class Produto
     public string? Nome { get; set; }
     public string? Descricao { get; set; }
     public decimal Preco { get; set; }
-    public string? ImagemURL { get; set; }
+    public string? ImagemUrl { get; set; }
     public float Estoque { get; set; }
     public DateTime DataCadastro { get; set; }
+    public int CategoriaId { get; set; }
+    public Categoria? Categoria { get; set; }
 
 }
