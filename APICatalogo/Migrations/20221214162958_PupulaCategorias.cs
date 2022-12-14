cdfe1f91937440f39a8bc0594facf6783924dfa0
Gustavo Figueiroa <gustavofsoledade@hotmail.com>
2022-12-14 14:21:09 -0300

    Populando as tabelas usando migration

---
+++ APICatalogo/Migrations/20221214162958_PupulaCategorias.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    public partial class PupulaCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into categorias(Nome, ImagemUrl) Values('Bebidas','bebidas.jpg')");
+            mb.Sql("Insert into categorias(Nome, ImagemUrl) Values('Lanches','lanches.jpg')");
+            mb.Sql("Insert into categorias(Nome, ImagemUrl) Values('Sobremesas','sobremesas.jpg')");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Categorias");
+        }
+    }
+}
